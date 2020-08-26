676b8395983b17f4025731ac2187be20c4bdb1a3
martinrebo <martinrg33@gmail.com>
2020-08-26 18:20:16 +0200

    :bug: Fix production bug

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
+
+app.use(bodyParser.json());
+
 app.use(express.static(path.join(__dirname, 'build')));
+
 
 app.get('/ping', function (req, res) {
  return res.send('pong');
