cdfc0274b84b3a2df3b21ff7bd540c0c31d14295
martinrebo <martinrg33@gmail.com>
2020-08-26 17:58:12 +0200

    :construction_worker: Create production build

---
+++ build/precache-manifest.53c15c23f61c072a2cb11eabe35c6d9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e48b33c0fd3d13163ccf52ae0de2e71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef73eae4bf17c2ccf40c",
+    "url": "/static/css/main.95abaffc.chunk.css"
+  },
+  {
+    "revision": "d006c8f517fc8e03ca41",
+    "url": "/static/js/2.3a53da16.chunk.js"
+  },
+  {
+    "revision": "5c26a3bbb4b1b0fd39402a8015dee937",
+    "url": "/static/js/2.3a53da16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef73eae4bf17c2ccf40c",
+    "url": "/static/js/main.f3fc0b4e.chunk.js"
+  },
+  {
+    "revision": "d92576271884ab955c96",
+    "url": "/static/js/runtime-main.bf856969.js"
+  }
+]);
